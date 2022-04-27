2ae1702d9d10c2191485d97b27a36019984b194b
mattgra <m.grass93@hotmail.com>
2022-04-27 12:55:19 +0200

    Added script to analysis words per char and season

---
+++ scripts/feature_words_per_character_and_season.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from core import preprocessing as prp
+import os
+
+if __name__ == '__main__':
+
+    # Load data
+    df = pd.read_csv("data/the-office_lines.csv", index_col=0)
+
+    # Preprocess data
+    df = prp.preprocessing_pipeline(df=df)
+
+    # Select data subset with characters of interest
+    characters = ['Michael', 'Dwight', 'Jim', 'Pam']
+    df_sub = df[df['character'].isin(characters)]
+
+    # Count number of lines per season and character and store in dataframe
+    dfg = df_sub.groupby(['season', 'character'])['line'].count().reset_index()
+
+    # Plot results
+    plt.ion()
+    fig, ax = plt.subplots()
+    sns.barplot(x='line', y='season', hue='character', hue_order=characters,
+                orient='h', palette='viridis', data=dfg, ax=ax)
+    ax.set_xlabel('Count of spoken lines')
+    ax.set_ylabel('Season')
+    ax.set_title('Count of spoken lines per season and character')
+
+    # Save figure
+    fp = 'docs/analysis_outputs/count_of_spoken_lines_per_season_and_character.png'
+    if not os.path.isfile(fp):
+        fig.savefig('docs/analysis_outputs/count_of_spoken_lines_per_season_and_character.png')
+
+    # Close figure
+    plt.close()
